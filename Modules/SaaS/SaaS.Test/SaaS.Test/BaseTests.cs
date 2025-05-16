1068a44e16fba1f99970095a735b817bec142d1e
Matheus Szoke <matheusszoke@gmail.com>
2025-05-16 15:29:04 -0300

    [feat] Add new project to make tests on SaaS project.
    NOTE: This test project have to exists in all modules.

---
+++ Modules/SaaS/SaaS.Test/SaaS.Test/BaseTests.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Database;
+using SaaS.Domain.Users;
+using System.Reflection;
+using System.Windows.Input;
+using SaaS.Api;
+
+namespace SaaS.Test;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
